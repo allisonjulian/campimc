fac171ad60f0545bec51cca6014ed49c745c661f
cdyap <cdyap@outlook.com>
2017-02-23 21:40:34 +0800

    DB fixes

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 	// $dbConnection = new PDO('mysql:dbname=campimc_actm;host=localhost;charset=utf8', 'root', 'root');
-	$dbConnection = new PDO('mysql:dbname=campimc_actm;host=localhost;charset=utf8', 'misaor5_campimc', 'm!54e53rV5');
+	$dbConnection = new PDO('mysql:dbname=misaor5_campimc;host=localhost;charset=utf8', 'misaor5_campimc', 'm!54e53rV5');
 
 	$dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
