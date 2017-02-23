aca4f3241cdbdae7f2a2ddd3aa644d101d777995
cdyap <cdyap@outlook.com>
2017-02-23 21:28:33 +0800

    rushfixes

--- submit.php
+++ submit.php
@@ -10,6 +10,8 @@
 			$contactnumber = $_POST['contactnumber'];
 			$email = $_POST['email'];
 			$nummem = $_POST['nummem'];
+			$pointperson = $_POST['pointperson'];
+			$pointcontact = $_POST['pointcontact'];
 			$firstmember = $_POST['firstmember'];
 			$secondmember = $_POST['secondmember'];
 			$thirdmember = $_POST['thirdmember'];
@@ -17,8 +19,8 @@
 			$fifthmember = $_POST['fifthmember'];
 
 
-			$statement = $dbConnection->prepare("INSERT INTO applications (teamname, school, contactnumber, email, nummem, firstmember, secondmember, thirdmember, fourthmember, fifthmember) VALUES (:teamname, :school, :contactnumber, :email, :nummem, :firstmember, :secondmember, :thirdmember, :fourthmember, :fifthmember)"); 
-			$statement->execute(array(':teamname' => $teamname, ':school' => $school, ':contactnumber' => $contactnumber, ':email' => $email, ':nummem' => $nummem, ':firstmember' => $firstmember, ':secondmember' => $secondmember, ':thirdmember' => $thirdmember, ':fourthmember' => $fourthmember, ':fifthmember' => $fifthmember));
+			$statement = $dbConnection->prepare("INSERT INTO applications (teamname, school, contactnumber, email, nummem, firstmember, secondmember, thirdmember, fourthmember, fifthmember, pointperson, pointcontact) VALUES (:teamname, :school, :contactnumber, :email, :nummem, :firstmember, :secondmember, :thirdmember, :fourthmember, :fifthmember, :pointperson, :pointcontact)"); 
+			$statement->execute(array(':teamname' => $teamname, ':school' => $school, ':contactnumber' => $contactnumber, ':email' => $email, ':nummem' => $nummem, ':firstmember' => $firstmember, ':secondmember' => $secondmember, ':thirdmember' => $thirdmember, ':fourthmember' => $fourthmember, ':fifthmember' => $fifthmember, ':pointperson' => $pointperson, ':pointcontact' => $pointcontact));
 				
 
 			$dbConnection = null;
